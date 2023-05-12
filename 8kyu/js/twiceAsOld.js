6055e3c01148e79dc73211f6176296426ce25826
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-05-11 17:07:18 -0700

    8kyu fundamentals

---
+++ 8kyu/js/twiceAsOld.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+// Twice as old
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
+function twiceAsOld(dadYearsOld, sonYearsOld){                  // First solution
+    // return Math.abs((sonYearsOld * 2) - dadYearsOld)
+    return Math.abs(dadYearsOld - (sonYearsOld * 2))
+}
+
+console.log(twiceAsOld(36, 7))
+console.log(twiceAsOld(55,30))
+console.log(twiceAsOld(42,21))
+console.log(twiceAsOld(22,1))
