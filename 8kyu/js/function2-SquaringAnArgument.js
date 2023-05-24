ecf641f2802e8fd7f9b859dc16eef0e6d7146324
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-05-23 17:13:23 -0700

    8kyu fundamentals

---
+++ 8kyu/js/function2-SquaringAnArgument.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/523b623152af8a30c6000027
+// Function 2 - squaring an argument
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+function square(n){
+    // return n * n            // First attempt
+
+    // return n **2            // Second attempt
+
+    return Math.pow(n, 2)        // Third attempt  
+}
