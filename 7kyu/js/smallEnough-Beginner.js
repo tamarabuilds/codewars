aa29f2ab3ccf865d9c4c13549a004245f933de05
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-07-01 20:22:19 -0700

    7kyu fundamentals

---
+++ 7kyu/js/smallEnough-Beginner.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/57cc981a58da9e302a000214
+// Small enough? - Beginner
+/*
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+*/
+
+// P: array of numbers, number limit
+// R: true if all values below the limit
+
+function smallEnough(a, limit){
+    // // loop
+    // for (let i = 0; i < a.length; i++){
+    //     // condition check each element
+    //     if (a[i] > limit) return false
+    // }
+    // return true
+
+    // // ... First solution
+    // // +: clear, clean, works
+    // // -: can do with a build in function?
+
+
+    // map
+    // return a.map( (e) => e <= limit ).includes(false) ? false: true
+
+    // // ... Second solution
+    // // +: 1 line
+    // // -: not as clear
+
+
+    return Math.max(...a) <= limit
+
+    // Learned solution
+
+
+}
+
+console.log(smallEnough([66, 101], 200))        // true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))        // false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107))        // true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120))        // true
