a176dcdaeacc345b8a5e890ba1fb6af35d53c055
tamarabuilds <98510821+tamarabuilds@users.noreply.github.com>
2023-08-03 23:27:25 -0400

    7kyu fundamentals

---
+++ 7kyu/js/factorial2.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212
+// Factorial
+/*
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+*/
+
+// P: integer
+// R: integer
+
+function factorial(n){
+    // // variables
+    // let answer = 1
+    // // loop
+    // for (let i = n; i >= 1; i-- ){
+    //     // multiply
+    //     answer *= i
+    // }
+    // return answer
+
+    // // first solution: clean. seems like something can make it more concise. 
+
+//    return n ? factorial(n - 1) * n : 1 
+
+    // learned solution: expensive but neat
+
+}
+
+console.log(factorial(0))   // 1
+console.log(factorial(1))   // 1
+console.log(factorial(4))   // 24
+console.log(factorial(7))   // 5040
