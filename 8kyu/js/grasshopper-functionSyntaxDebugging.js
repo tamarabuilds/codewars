fc87ad71f836da5a3e14e6e2db8a8944ac73c97c
tamarabuilds <98510821+tamarabuilds@users.noreply.github.com>
2023-11-22 13:05:17 -0800

    8kyu fundamentals

---
+++ 8kyu/js/grasshopper-functionSyntaxDebugging.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/javascript
+Grasshopper - Function syntax debugging
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main [verb, noun]
+  return verb + noun
+}
+
+*/
+
+//PREP
+
+// Parameters: provided broken code
+// Returns: Working code
+
+function main (verb, noun) {
+    return verb + noun
+}
+  
+
+
+// EXAMPLES
+console.log(main('take ', 'item')) // "take item"
