9fcdfa78b09345851702576c0855ee63173c790e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-09-12 05:37:16 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -29,14 +29,25 @@ console.log("main.js loaded");
 
 
 
+////////////////////////All Star Code Challenge #18
+/*
+function strCount(str, letter){  
+  //code here
+  return str.split(letter).length - 1
+}
 
+console.log(strCount('Hello', 'o'), 1)
+console.log(strCount('Hello', 'l'), 2)
+console.log(strCount('', 'z'), 0)
+*/
 
 
 ///////////////////////////Grasshopper - Check for factor
+/*
 function checkForFactor (base, factor) {
   return base % factor === 0;
 }
-
+*/
 
 ///////////////////////////////Keep up the hoop
 /*
