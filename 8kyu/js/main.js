513856bde632a3e0440ff3cf0a5e0ee79495e443
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-09-16 20:56:20 -0700

    test commit

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -9,6 +9,7 @@ console.log('main.js loaded');
 
 
 
+
 //https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 /*
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
