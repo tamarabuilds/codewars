15949c8b2868facbad1c878359bb7eda6cee397c
Tamara Patterson <tamara_patterson@intuit.com>
2024-09-16 20:23:15 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -2,8 +2,31 @@ console.log('main.js loaded');
 
 
 // PREP
-// PARAMETERS: string with numbes, letters, and special characters
+// PARAMETERS: string with numbers, letters, and special characters
 // RETURN: string with spacing, no numbers
 // EXAMPLES
 // PSEUDOCODE
 
+
+
+
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+// PREP
+// PARAMETERS: integer
+// RETURN: "Even" or "Odd"
+// EXAMPLES
+// PSEUDOCODE
+
+function evenOrOdd(number){
+    return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(4), "Even")
+console.log(evenOrOdd(0), "Even")
+console.log(evenOrOdd(7), "Odd")
+
+*/
