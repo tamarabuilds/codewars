de540d8bcf42a582729ee06f05f95d4a35007b2e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-09-18 07:32:23 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -1,5 +1,4 @@
-console.log('main.js loaded');
-
+console.log("main.js loaded");
 
 // PREP
 // PARAMETERS: string with numbers, letters, and special characters
@@ -7,7 +6,39 @@ console.log('main.js loaded');
 // EXAMPLES
 // PSEUDOCODE
 
+// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+/*
+You get an array of numbers, return the sum of all of the positives ones.
 
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+// Parameters: array of integers
+// Examples:
+// Return: sum of positive integers
+// Pseudocode:
+
+/*
+function positiveSum(arr) {
+  // variable
+  let sum = 0;
+  // if positive add
+  arr.map((num) => {
+    if (num > 0) {
+      sum = sum + num;
+    }
+  });
+  return sum;
+}
+
+console.log(positiveSum([1, 2, 3, 4, 5]), 15);
+console.log(positiveSum([1, -2, 3, 4, 5]), 13);
+console.log(positiveSum([]), 0);
+console.log(positiveSum([-1, -2, -3, -4, -5]), 0);
+console.log(positiveSum([-1, 2, 3, 4, -5]), 9);
+
+*/
 
 //https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 /*
