bd9ceeb60c8a28466a9a7d479d9d384d51cccdb5
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-09-26 06:47:40 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -15,6 +15,37 @@ console.log("main.js loaded");
 
 
 
+
+
+
+
+
+/////////////////////////Abbreviate a Two Word Name
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+/*
+function abbrevName(name){
+  const firstInitial = name[0].toUpperCase()
+  const lastInitial = name.split(' ')[1][0].toUpperCase()
+  return `${firstInitial}.${lastInitial}`
+}
+
+console.log(abbrevName("Sam Harris"), "S.H")
+console.log(abbrevName("P Favuzzi"), "P.F")
+*/
+
+
+
 //////////////////////////////Convert a String to a Number!
 
 /*
