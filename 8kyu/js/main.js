b6d79c4f6ffc44102c67ecb7f7ecec3d3d8ee099
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-01 06:01:44 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -29,7 +29,14 @@ console.log("main.js loaded");
 
 
 
+//////////////////// Returning Strings
 
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet('Ryan'), "Hello, Ryan how are you doing today?")
+console.log(greet('Shingles'), "Hello, Shingles how are you doing today?")
 
 
 ////////////////////// Convert number to reversed array of digits
