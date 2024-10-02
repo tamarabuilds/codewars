594e88997c28b592aa7162fabcb29cf2b28b927b
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-02 06:07:51 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -25,19 +25,37 @@ console.log("main.js loaded");
 
 
 
+///////////////////// Opposites Attract
+/*
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+  const isFlower1even = flower1 % 2 === 0;
+  const isFlower2even = flower2 % 2 === 0;
+  return isFlower1even !== isFlower2even
+}
+
+console.log(lovefunc(1,4), true)
+console.log(lovefunc(2,2), false)
+console.log(lovefunc(0,1), true)
+console.log(lovefunc(0,0), false)
 
 
 
 
 //////////////////// Returning Strings
-
+/*
 function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
 
 console.log(greet('Ryan'), "Hello, Ryan how are you doing today?")
 console.log(greet('Shingles'), "Hello, Shingles how are you doing today?")
-
+*/
 
 ////////////////////// Convert number to reversed array of digits
 /*
