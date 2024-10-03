f76c4a8e2bcbb329d3cc623932a978a109d0bd8e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-03 06:10:20 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -25,13 +25,22 @@ console.log("main.js loaded");
 
 
 
+
+
+//////////////////// Function 1 - hello world
+/*
+function greet() {
+  return "hello world!";
+  }
+
+*/
 ///////////////////// Opposites Attract
 /*
 Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
 
 Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 */
-
+/*
 function lovefunc(flower1, flower2){
   // moment of truth
   const isFlower1even = flower1 % 2 === 0;
@@ -43,7 +52,7 @@ console.log(lovefunc(1,4), true)
 console.log(lovefunc(2,2), false)
 console.log(lovefunc(0,1), true)
 console.log(lovefunc(0,0), false)
-
+*/
 
 
 
