b081bd03143c7ecbcf4948b901dccc8985f69d2e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-04 06:19:46 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -19,9 +19,24 @@ console.log("main.js loaded");
 
 
 
+//////////////// Beginner - Lost Without a Map
+/*
+Given an array of integers, return a new array with each value doubled.
 
+For example:
 
+[1, 2, 3] --> [2, 4, 6]
+*/
 
+/*
+function maps(x){
+  return x.map(num => num * 2)
+}
+
+console.log(maps([1, 2, 3]), [2, 4, 6])
+console.log(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8])
+console.log(maps([2, 2, 2, 2, 2, 2]), [2, 2, 2, 2, 2, 2])
+*/
 
 //////////////// A Needle in the Haystack
 /*
