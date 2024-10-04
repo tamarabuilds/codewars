c84ba0e14136bc8b1ecb8e98e624e0cb9b2e2823
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-04 06:15:26 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -8,7 +8,7 @@ console.log("main.js loaded");
 
 // https://www.codewars.com/kata/search/my-languages?q=&r%5B%5D=-8&tags=Fundamentals&beta=false&order_by=satisfaction_percent%20desc%2Ctotal_completed%20desc
 
-/////////////// LAST: page 1:
+/////////////// 
 
 
 
@@ -23,8 +23,37 @@ console.log("main.js loaded");
 
 
 
+//////////////// A Needle in the Haystack
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+*/
+
+/*
+function findNeedle(haystack) {
+  // your code here
+  const position = haystack.indexOf('needle');
+  return `found the needle at position ${position}`
+}
 
 
+var haystack_1 = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
+var haystack_2 = ['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'];
+var haystack_3 = [1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54];
+
+console.log(findNeedle(haystack_1), "found the needle at position 3")
+console.log(findNeedle(haystack_2), "found the needle at position 5")
+console.log(findNeedle(haystack_3), "found the needle at position 30")
+*/
 
 
 //////////////////// Function 1 - hello world
