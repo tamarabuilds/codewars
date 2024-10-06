58b31bf005b1b5b9ea3791eccf295484e57b77f3
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-06 07:55:32 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -28,6 +28,36 @@ console.log("main.js loaded");
 
 
 
+/////////////////////// How good are you really?
+/*There was a test in your class and you passed it. Congratulations!
+
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+*/
+
+
+/*
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  const countClass = classPoints.length
+  const sumClass = classPoints.reduce((acc, num) => acc + num, 0)
+  return sumClass / countClass < yourPoints 
+}
+
+
+console.log(betterThanAverage([2, 3], 5), true)
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true)
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false)
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false)
+
+*/
 
 
 ///////////////////// Beginner Series #2 Clock
