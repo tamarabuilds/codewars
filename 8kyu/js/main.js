1978c71323bbeef794891c7c14c0a6a5297e0137
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-07 08:48:36 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -19,6 +19,44 @@ console.log("main.js loaded");
 
 
 
+
+
+
+
+
+
+
+
+//////////////////////// Fake Binary
+/*
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+/*
+function fakeBin(x){
+  const arr = x.split('')
+  // let result = []
+  // arr.forEach( n => {
+  //   if (n < 5) { result.push('0')}
+  //   if (n >= 5) { result.push('1')}
+  // })
+ return arr.map( n => n < 5 ? '0' : '1').join('')
+}
+
+console.log(fakeBin('45385593107843568'), '01011110001100111')
+console.log(fakeBin('509321967506747'), '101000111101101')
+console.log(fakeBin('366058562030849490134388085'), '011011110000101010000011011')
+*/
+/////////////// MakeUpperCase
+/*
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase()
+}
+*/
+
 /////////////////// Count of positives / sum of negatives
 
 /*
