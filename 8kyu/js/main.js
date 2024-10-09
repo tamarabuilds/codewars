dfc64db2242f93683cd5cb7dfe1e966094475f8a
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-09 06:37:22 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -18,6 +18,44 @@ console.log("main.js loaded");
 
 
 
+
+
+
+
+
+////////////////////// Beginner - Reduce but Grow
+
+function grow(x) {
+  return x.reduce( (acc, c) => acc * c, 1)
+}
+
+console.log(grow([1, 2, 3]), 6)
+console.log(grow([4, 1, 1, 1, 4]), 16)
+
+//////////////////// Calculate BMI
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+/*
+function bmi(weight, height) {
+  const bmi = weight / (height * height);
+  if (bmi <= 18.5) return "Underweight"
+  if (bmi <= 25.0) return "Normal"
+  if (bmi <= 30.0) return "Overweight"
+  if (bmi > 30.0) return "Obese"  
+}
+
+console.log(bmi(80, 1.80), "Normal")
+*/
+
 //////////////////////Sentence Smash
 /*
 function smash (words) {
