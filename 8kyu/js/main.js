4334796de50e6a1a480c042460ce52d172371c7d
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-11 06:55:36 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -16,17 +16,42 @@ console.log("main.js loaded");
 
 
 
+//////////////////////////// DNA to RNA Conversion
 
+/*
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+/*
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  // return dna.split('').map((element) => element === 'T' ? 'U': element).join('')
+  // return dna.replaceAll('T', 'U')
+  return dna.split('T').join('U')
+}
+
+console.log(DNAtoRNA('TTTT'), 'UUUU')
+console.log(DNAtoRNA('GCAT'), 'GCAU')
+console.log(DNAtoRNA('GACCGCCGCC'), 'GACCGCCGCC')
+*/
 
 ////////////////////////////Is he gonna survive?
-
+/*
 function hero(bullets, dragons){
   return bullets / 2 >= dragons;
   }
 
   console.log(hero(10, 5), true)
   console.log(hero(7, 4), false)
-
+*/
 
 /////////////////////// Reversed sequence
 /*
