af397c325fe7f4e7895c80cd926f74ee69c885db
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-23 13:57:54 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -19,8 +19,20 @@ console.log("main.js loaded");
 
 
 
+///////////////////Will there be enough space?
 
+/*
 
+function enough(cap, on, wait) {
+  // your code here
+  return cap >= (on + wait) ? 0 : on + wait - cap
+}
+
+console.log(enough(10, 5, 5), 0)
+console.log(enough(100, 60, 50), 10)
+console.log(enough(20, 5, 5), 0)
+
+*/
 
 ////////////////////Third Angle of a Triangle
 /*
