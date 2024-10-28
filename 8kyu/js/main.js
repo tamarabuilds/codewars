6e3999514d9126e576c3264d817efe584f3f78bf
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-10-28 06:52:09 -0700

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -16,6 +16,32 @@ console.log("main.js loaded");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////Keep up the hoop
+/*
+function hoopCount (n) {
+  return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+}
+*/
+
 ////////////////////////Beginner Series #4 Cockroach
 
 /*
