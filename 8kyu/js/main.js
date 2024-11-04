5afd6140dff500f8c88ba07cfda19d941b37a616
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-11-03 18:26:09 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -23,8 +23,28 @@ console.log("main.js loaded");
 
 
 
+///////////////Cat years, Dog years
+/*
+var humanYearsCatYearsDogYears = function(humanYears) {
+  // Your code here!
+  let catYears = 15;
+  let dogYears = 15;
+  if (humanYears > 1) {
+    catYears += 9
+    dogYears += 9
+  }
+  if (humanYears > 2) {
+    catYears += (humanYears-2) * 4
+    dogYears += (humanYears-2) * 5
+  }
 
+  return [humanYears, catYears, dogYears];
+}
 
+console.log(humanYearsCatYearsDogYears(1), [1, 15, 15])
+console.log(humanYearsCatYearsDogYears(2), [2, 24, 24])
+console.log(humanYearsCatYearsDogYears(10), [10, 56, 64])
+*/
 
 /////////////////Switch it Up!
 /*
