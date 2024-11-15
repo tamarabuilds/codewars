1865e5e55f803689bcddf408d54e99848df8f6f0
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-11-14 16:38:03 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -13,6 +13,38 @@ console.log("main.js loaded");
 /////////////// Welcome!
 /*
  */
+/////////////// Welcome!
+/*
+ */
+/////////////// Welcome!
+/*
+ */
+/////////////// Welcome!
+/*
+ */
+/////////////// Welcome!
+/*
+ */
+/////////////// Welcome!
+/*
+ */
+/////////////// Welcome!
+/*
+ */
+
+/////////////// Welcome!
+/*
+ */
+/////////////// Sort and Star
+/*
+function twoSort(s) {
+  return s.sort()[0].split('').join('***')
+}
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n')
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e')
+*/
+
 /////////////// Grasshopper - Terminal game move function
 
 /*
