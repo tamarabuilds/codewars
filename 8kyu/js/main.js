ed7d0cd835676fd0fa995810eae939a31e097b5f
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-11-15 16:43:36 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -32,9 +32,21 @@ console.log("main.js loaded");
 /*
  */
 
-/////////////// Welcome!
+/////////////// Training JS #7: if..else and ternary operator
+
 /*
- */
+function saleHotdogs(n){
+  return n > 9 ? n * 90 : n < 5 ? n * 100 : n * 95
+}
+
+console.log(saleHotdogs(1), 100)
+console.log(saleHotdogs(4), 400)
+console.log(saleHotdogs(5), 475)
+console.log(saleHotdogs(9), 855)
+console.log(saleHotdogs(10), 900)
+console.log(saleHotdogs(100), 9000)
+*/
+
 /////////////// Sort and Star
 /*
 function twoSort(s) {
