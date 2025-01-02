6798c75ce52bb5dc5360aa4bce53ec1c2a4a222e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2025-01-01 16:28:56 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -13,10 +13,18 @@ console.log("main.js loaded");
 /////////////// Welcome!
 /*
  */
-/////////////// Grasshopper - Messi goals function
+/////////////// Grasshopper - Terminal game move function
 
 /*
 
+function move (position, roll) {
+  // return the new position
+  return position + roll * 2
+}
+
+console.log(move(0, 4), 8)
+console.log(move(3, 6), 15)
+console.log(move(2, 5), 12)
 */
 
 /////////////// Difference of Volumes of Cuboids
