85432a23bdf9a74d94d4720565ded6b9808a4a5c
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2025-01-18 13:43:57 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -28,8 +28,14 @@ console.log("main.js loaded");
 /////////////// Welcome!
 /*
  */
-/////////////// Welcome!
+/////////////// Training JS #1: create your first JS function and print "Hello World!"
+
 /*
+//refer to the example and write your first JS function
+function helloWorld(){
+  const str = 'Hello World!'
+  console.log(str)
+}
  */
 /////////////// Lario and Muigi Pipe Problem
 
