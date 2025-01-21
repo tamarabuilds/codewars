d801e9e40c8ee66a9780a9e07520b0117645837d
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2025-01-20 21:16:02 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -25,9 +25,21 @@ console.log("main.js loaded");
 /////////////// Welcome!
 /*
  */
-/////////////// Welcome!
+/////////////// What's the real floor?
+
 /*
- */
+function getRealFloor(n) {
+  if (n < 1) return n
+  if (n < 13) return n - 1
+  return n - 2
+}
+
+console.log(getRealFloor(1), 0)
+console.log(getRealFloor(5), 4)
+console.log(getRealFloor(15), 13)
+console.log(getRealFloor(-2), -2)
+*/
+
 /////////////// Training JS #1: create your first JS function and print "Hello World!"
 
 /*
