4532e4ba110dd4f54cfb3abc2ecea161ede389d1
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2025-01-24 15:24:17 -0800

    8kyu

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -55,9 +55,19 @@ console.log("main.js loaded");
 /////////////// Welcome
 /*
  */
-/////////////// Welcome
+/////////////// Merge two sorted arrays into one
 /*
- */
+
+function mergeArrays(arr1, arr2) {
+  const result =  new Set([...arr1, ...arr2].sort( (a, b)=> a - b))
+  return [...result]
+}
+
+console.log(mergeArrays([1,2,3,4], [5,6,7,8]), [1,2,3,4,5,6,7,8])
+console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]), [1,2,3,4,5,6,7,8,9,10])
+*/
+
+
 /////////////// Reversing Words in a String
 /*
 
